08782b3771ab198492b2b0786889e1155c02dbe5
nuclearpinguin <turbaszek@gmail.com>
2019-09-14 17:02:13 +0200

    feat: magic maps

---
+++ api/image_map.py
@@ -0,0 +1,21 @@
+from api.tags import *
+
+IMAGE_MAP = {
+    "city1.jpg": [CITY, HOTEL, ARCHITECTURE],
+    "city3.jpg": [CITY, HOTEL, ARCHITECTURE],
+    "city4.jpg": [CITY, HOTEL, EXPERIENCE, CULTURE],
+    "desert1.jpg": [WILDERNESS, EXPERIENCE, HOT],
+    "desert2.jpg": [WILDERNESS, EXPERIENCE, LANDSCAPE, HOT],
+    "food1.jpg": [FOOD, HOTEL],
+    "food2.jpg": [FOOD, EXPERIENCE, HOTEL],
+    "food3.jpg": [FOOD, EXPERIENCE, CULTURE],
+    "sea1.jpg": [SEA, EXPERIENCE, HOT],
+    "sea2.jpg": [SEA, EXPERIENCE, HOT],
+    "exp1.jpg": [EXPERIENCE, LANDSCAPE, CULTURE],
+    "lake1.jpg": [EXPERIENCE, LANDSCAPE, HOTEL, TENT, LAKE],
+    "lake2.jpg": [EXPERIENCE, LANDSCAPE, LAKE],
+    "mountain1.jpg": [MOUNTAIN, EXPERIENCE, LAKE, LANDSCAPE],
+    "mountain2.jpg": [MOUNTAIN, EXPERIENCE, LANDSCAPE, COLD],
+    "mountain3.jpg": [MOUNTAIN, EXPERIENCE, LANDSCAPE, COLD, TENT],
+    "tent2.jpg": [EXPERIENCE, WILDERNESS, TENT, LANDSCAPE],
+}
