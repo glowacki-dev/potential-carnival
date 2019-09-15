b1409d72c2cfff48c8643dbb8de12fb7f1aca46e
nuclearpinguin <turbaszek@gmail.com>
2019-09-15 04:06:33 +0200

    add backslash

--- client/src/data/session/client.js
+++ client/src/data/session/client.js
@@ -4,7 +4,7 @@ const fetch = require('node-fetch');
 
 export class sessionClient extends BaseClient {
   obtainSessionID = async () => {
-    let session = await fetch(this._url() + 'sessions', { method: 'POST' });
+    let session = await fetch(this._url() + 'sessions/', { method: 'POST' });
 
     let session_body = await session.json();
     return session_body['session_id'];
