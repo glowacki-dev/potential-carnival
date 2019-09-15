878a9d5e8e4a624d867a8aced53189c5a8710047
Maciej Głowacki <maciej.glowacki@daftcode.pl>
2019-09-15 06:00:36 +0200

    Add some new styles

--- client/src/components/FullLoader.js
+++ client/src/components/FullLoader.js
@@ -3,8 +3,22 @@ import { makeStyles } from '@material-ui/core/styles';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
 const useStyles = makeStyles(theme => ({
+  overlay: {
+    position: 'absolute',
+    zIndex: 999,
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    background:
+      'linear-gradient(161deg, rgba(78,113,163,1) 0%, rgba(202,209,237,1) 100%)',
+    color: '#FFF',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
   progress: {
-    margin: theme.spacing(2)
+    margin: theme.spacing(5)
   }
 }));
 
@@ -12,8 +26,13 @@ export default function FullLoader() {
   const classes = useStyles();
 
   return (
-    <div>
-      <CircularProgress className={classes.progress} size={100} />
+    <div className={classes.overlay}>
+      <img width="300" src={require('../logo.svg')} />
+      <CircularProgress
+        className={classes.progress}
+        disableShrink="true"
+        size={100}
+      />
     </div>
   );
 }
